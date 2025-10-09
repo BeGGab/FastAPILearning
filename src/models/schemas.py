3664bce008bfcfe5789c9ec57e3713abd6fbca02
BeGGab <123666399+BeGGab@users.noreply.github.com>
2025-10-09 18:41:27 +0300

    Update schemas.py

--- src/models/schemas.py
+++ src/models/schemas.py
@@ -8,19 +8,22 @@ from datetime import datetime
 
 
 class SAutorAdd(BaseModel):
-    id: uuid.UUID = Field(None, description="ID автора")
+    model_config = ConfigDict(from_attributes=True)
+    id: int = Field(..., description="ID автора")
     name: str = Field(..., min_length=3, max_length=50, description="Имя автора")
     bio: Optional[str] = Field(None, max_length=1000, description="Биография автора")
 
 class SBookAdd(BaseModel):
-    id: uuid.UUID = Field(None, description="ID книги")
+    model_config = ConfigDict(from_attributes=True)
+    id: int = Field(..., description="ID книги")
     title: Optional[str] = Field(None, min_length=5, max_length=100, description="Название книги")
     description: Optional[str] = Field(None, max_length=1000, description="Описание книги")
     publication_year: Optional[int] = Field(None, description="Год публикации")
     
 
 class SDepartmentAdd(BaseModel):
-    id: uuid.UUID = Field(None, description="ID отдела")
+    model_config = ConfigDict(from_attributes=True)
+    id: int = Field(..., description="ID отдела")
     name: Optional[str] = Field(None, min_length=3, max_length=50, description="Название отдела")
     description: Optional[str] = Field(None, max_length=1000, description="Описание отдела")
 
@@ -28,7 +31,8 @@ class SDepartmentAdd(BaseModel):
 
 
 class SProfileAdd(BaseModel):
-    id: uuid.UUID = Field(None, description="ID пользователя")
+    model_config = ConfigDict(from_attributes=True)
+    id: uuid.UUID = Field(..., description="ID пользователя", defaut=uuid.uuid4())
     full_name: str = Field(..., min_length=3, max_length=50, description="Полное имя пользователя")
     bio: str = Field(..., max_length=1000, description="Биография пользователя")
 
